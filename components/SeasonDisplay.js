5352817acb7924600ba01a874c7a572843f69329
axelobscura <axosar@gmail.com>
2020-10-06 09:35:39 -0500

    Getting window object in component

--- components/SeasonDisplay.js
+++ components/SeasonDisplay.js
@@ -4,19 +4,20 @@ import React, { Component } from 'react';
 class SeasonDisplay extends Component {
     constructor(props){
         super(props);
-
         this.state = {
             lat: null
         };
-
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({
-                    lat: position.coords.latitude
-                })
-            },
-            (err) => console.log(err)
-        );
+        if (typeof window !== "undefined") {
+            window.navigator.geolocation.getCurrentPosition(
+                position => {
+                    this.setState({
+                        lat: position.coords.latitude
+                    })
+                },
+                (err) => console.log(err)
+            );
+        }
+        
     }
     render(){
         
