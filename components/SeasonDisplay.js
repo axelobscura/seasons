f41df6a9f1c1a32066609486a6db3cbc44e7d14c
axelobscura <axosar@gmail.com>
2020-10-08 08:22:22 -0500

    Adding componentDidMount

--- components/SeasonDisplay.js
+++ components/SeasonDisplay.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 class SeasonDisplay extends Component {
     constructor(props){
         super(props);
@@ -8,22 +7,21 @@ class SeasonDisplay extends Component {
             lat: null,
             errorMessage: ''
         };
+    }
+
+    componentDidMount(){
         if (typeof window !== "undefined") {
             window.navigator.geolocation.getCurrentPosition(
-                position => {
-                    this.setState({
-                        lat: position.coords.latitude
-                    })
-                },
-                (err) => {
-                    this.setState({
-                        errorMessage: err.message
-                    })
-                }
+                position => this.setState({lat: position.coords.latitude}),
+                (err) => this.setState({errorMessage: err.message})
             );
         }
-        
     }
+
+    componentDidUpdate(){
+        console.log("My component did update");
+    }
+
     render(){
         if(this.state.errorMessage && !this.state.lat){
             return <div><h1>Error: {this.state.errorMessage}</h1></div>
