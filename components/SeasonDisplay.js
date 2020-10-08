f878f7bc1f7beb1f70eb5a417d4217941cbcbb24
axelobscura <axosar@gmail.com>
2020-10-08 08:26:21 -0500

    Refactoring STATE

--- components/SeasonDisplay.js
+++ components/SeasonDisplay.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 
 class SeasonDisplay extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        };
-    }
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount(){
         if (typeof window !== "undefined") {
